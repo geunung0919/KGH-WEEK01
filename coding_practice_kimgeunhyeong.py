edf88d4c0b191e879823e31395eef7a284a3b37b
김근형 <nhunk94@gmail.com>
2026-03-11 12:45:48 +0900

    Updated Changes

--- coding_practice_kimgeunhyeong.py
+++ coding_practice_kimgeunhyeong.py
@@ -33,4 +33,22 @@ a = "Now is better than never"
 print(a[0])
 print(a[4])
 print(a[-1])
+print(a[-2])
+
+#문자열 슬라이싱
+b = a[0] + a[1] + a[2]
+print(b)
+
+print(a[4:6])
+print(a[19:])
+print(a[:3])
+print(a[7: -11])
+
+#문자 개수 계산
+a = "Python"
+print(a.count('p'))
+
+#문자 위치 확인
+print(a.find('y'))
+print(a.find('p'))
 
