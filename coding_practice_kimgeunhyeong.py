09f6c73baa3479d73e8f06b0db994b79df8b9098
김근형 <nhunk94@gmail.com>
2026-03-11 13:13:48 +0900

    Updated Changes

--- coding_practice_kimgeunhyeong.py
+++ coding_practice_kimgeunhyeong.py
@@ -266,3 +266,9 @@ money = 1300
 if money >= 1200 and money < 3500:
     print('버스를 탈 수 있습니다.')
 
+#그룹 자료형의 원소인지 검사하기
+print(1 in [1,2,3])
+print(x in [1,2,3])
+print(x not in [1,2,3])
+print('a' in ['a', 'b', 'c', 'd'])
+print('i' not is 'Python')
