a45c27fcc44f0ef0a1c303862d510e1e7f1342f3
김근형 <nhunk94@gmail.com>
2026-03-11 13:39:18 +0900

    Updated Changes

--- coding_practice_kimgeunhyeong.py
+++ coding_practice_kimgeunhyeong.py
@@ -451,4 +451,17 @@ lines = f.readline()
 print(lines)
 
 for line in lines:
-    
+    print(line)
+
+f.close()
+
+#파일 모드 'r' - read()
+f = open('example.txt', 'r')
+data = f.read()
+f.close()
+data
+
+#with open() as 파일 객체
+with open('example.txt', 'w') as f:
+    f.write("Now is better than never.")
+#data = f.read()        #오류발생 
