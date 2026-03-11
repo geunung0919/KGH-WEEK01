37aabd4bd89457e2f7b5009bad790bbfbdaceec7
김근형 <nhunk94@gmail.com>
2026-03-11 13:20:48 +0900

    Updated Changes

--- coding_practice_kimgeunhyeong.py
+++ coding_practice_kimgeunhyeong.py
@@ -295,4 +295,37 @@ for score in [90,25,67,45,93]:
     if score > 60:
         print("%d번 학생은 합격입니다." % number)
     else:
-        print("%d번 학생은 불합격입니다.")
+        print("%d번 학생은 불합격입니다." % number)
+
+#while 문
+i= 0
+while i < 5:
+    i += 1
+    print('*' * i)
+
+#함수 정의
+def sum1(a,b):
+    x = a+b
+    return x
+
+def sum2(*args):
+    x = 0 
+    for i in args:
+        x += i
+    return x
+
+#함수 호출
+a = 5
+b = 3
+print(sum1(a,b))
+print(sum1(3,5))
+print(sum2(1,2,3,4,5))
+print(sum2(2,3.5,10))
+
+"""## 내장 함수"""
+
+#숫자 x의 절대값을 반환
+print(abs(-3.5))
+
+#그룹 자료형의 변수 x의 모든 원소가 참(0이 아닌 값)이면 True 반환
+print(all([1,2,3,4]))
