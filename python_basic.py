dbe44ab5dfd7538a385fadb055d5d9e728f5fc8e
김근형 <nhunk94@gmail.com>
2026-03-15 17:04:24 +0900

    Updated Changes

--- python_basic.py
+++ python_basic.py
@@ -106,3 +106,17 @@ print(b[0] + " hi~^^;")
 
 #리스트 반복
 print(a * 3)
+
+#리스트 수정
+a[2] = 99
+print(a)
+
+a[1:2] = ['a','b','c']
+print(a)
+
+a[-1] = ['d','e','f']
+print(a)
+
+# 삭제
+del a[-1]
+print(a)
