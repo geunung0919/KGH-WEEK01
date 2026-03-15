3b61ad83095ecb86524c5112b82f4c6c716341e8
김근형 <nhunk94@gmail.com>
2026-03-15 17:13:54 +0900

    Updated Changes

--- python_basic.py
+++ python_basic.py
@@ -198,4 +198,22 @@ print(dic['Phone'])
 print(dic['name'])
 
 #key의 리스트 만들기 
-print(dic,keys())
+print(dic.keys())
+print(list(dic.keys()))
+
+for key in dic.keys():
+    print(dic[key])
+
+#value 의 리스트 만들기
+print(dic.values())
+print(list(dic.values()))
+
+#key, value 쌍 구하기
+print(dic.items())
+
+for key,value in dic.items():
+    print(key + ":" + value)
+
+#원소 삭제
+dic.clear()
+print(dic)
